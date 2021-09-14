92225ca86435089381d2b568c2febad001878bd9
Burak <wizard1456@hotmail.com>
2021-09-14 20:56:03 +0300

    Cursor bugs fixed: 0 index added

--- src/mixins/caret.js
+++ src/mixins/caret.js
@@ -19,15 +19,19 @@ export default {
         cursorSize: {
             type: [String, Number],
             default: 14
+        },
+        cursorOpacity: {
+            type: [String, Number],
+            default: 50
         }
     },
     data() {
         return {
             splitCmd: [
-                `<span><div style="opacity: 50%;float: right;width: ${this.cursorWidth}px;height: ${this.cursorSize}px;background: ${this.cursorColor};display: inline;" id='cursor'></div></span>`
+                `<span><div style="opacity: ${this.cursorOpacity}%;float: right;width: ${this.cursorWidth}px;height: ${this.cursorSize}px;background: ${this.cursorColor};display: inline;" id='cursor'></div></span>`
             ],
-            cursor: `opacity: 50%;float: right;width: ${this.cursorWidth}px;height: 14px;background: #21f838;display: inline;`,
-            marginLeft: this.cursorWidth,
+            cursor: `opacity: ${this.cursorOpacity}%;float: right;width: ${this.cursorWidth}px;height: ${this.cursorSize}px;background: ${this.cursorColor};display: inline;`,
+
             cursorPositionIndex: 0,
         }
     },
@@ -39,8 +43,8 @@ export default {
                     if (this.splitCmd[i].includes('cursor')) {
                         index = i;
                         this.splitCmd[i - 1] = this.splitCmd[i - 1].slice(this.splitCmd[i - 1].lastIndexOf('<span>'), this.splitCmd[i - 1].lastIndexOf('</span>')) + `<div style="${this.cursor}" id='cursor'></div>` + this.splitCmd[i - 1].substr(this.splitCmd[i - 1].lastIndexOf('</span>'))
-                        this.splitCmd[i] = `<span style="margin-left: -${this.marginLeft}px">` + (this.splitCmd[i].replace(this.splitCmd[i].slice(this.splitCmd[i].indexOf('<div'), this.splitCmd[i].lastIndexOf('</span>')), '')).slice(6, 7) + '</span>'
-                        if (this.splitCmd[i + 1]) this.splitCmd[i + 1] = '<span>' + this.splitCmd[i + 1].slice(this.splitCmd[i + 1].indexOf('>')+1)
+                        this.splitCmd[i] = `<span style="margin-left: -${this.cursorWidth}px">` + (this.splitCmd[i].replace(this.splitCmd[i].slice(this.splitCmd[i].indexOf('<div'), this.splitCmd[i].lastIndexOf('</span>')), '')).slice(6, 7) + '</span>'
+                        if (this.splitCmd[i + 1]) this.splitCmd[i + 1] = '<span>' + this.splitCmd[i + 1].slice(this.splitCmd[i + 1].indexOf('>') + 1)
                         this.cursorPositionIndex = i;
                         return;
                     }
@@ -52,9 +56,9 @@ export default {
                 if (this.splitCmd[i + 1])
                     if (this.splitCmd[i].includes('cursor')) {
                         index = i;
-                        if(this.splitCmd[i+1].includes('margin-left')) this.splitCmd[i + 1] = '<span>' + this.splitCmd[i + 1].slice(this.splitCmd[i + 1].indexOf('>')+1)
+                        if (this.splitCmd[i + 1].includes('margin-left')) this.splitCmd[i + 1] = '<span>' + this.splitCmd[i + 1].slice(this.splitCmd[i + 1].indexOf('>') + 1)
                         this.splitCmd[i + 1] = this.splitCmd[i + 1].slice(this.splitCmd[i + 1].lastIndexOf('<span>'), this.splitCmd[i + 1].lastIndexOf('</span>')) + `<div style="${this.cursor}" id='cursor'></div>` + this.splitCmd[i + 1].substr(this.splitCmd[i + 1].lastIndexOf('</span>'))
-                        if (this.splitCmd[i + 2]) this.splitCmd[i + 2] = `<span style="margin-left: -${this.marginLeft}px;">` + (this.splitCmd[i + 2].replace(this.splitCmd[i + 2].slice(this.splitCmd[i + 2].indexOf('<div'), this.splitCmd[i + 2].lastIndexOf('</span>')), '')).slice(6, 7) + '</span>'
+                        if (this.splitCmd[i + 2]) this.splitCmd[i + 2] = `<span style="margin-left: -${this.cursorWidth}px;">` + (this.splitCmd[i + 2].replace(this.splitCmd[i + 2].slice(this.splitCmd[i + 2].indexOf('<div'), this.splitCmd[i + 2].lastIndexOf('</span>')), '')).slice(6, 7) + '</span>'
                         this.splitCmd[i] = this.splitCmd[i].replace(this.splitCmd[i].slice(this.splitCmd[i].indexOf('<div'), this.splitCmd[i].lastIndexOf('</span>')), '')
                         this.cursorPositionIndex = i;
                         return;
@@ -64,10 +68,11 @@ export default {
         append(e) {
             if ((e.keyCode > 64 && e.keyCode < 91) || e.keyCode == 32 || (e.keyCode > 48 && e.keyCode < 57)) {
                 let span = `<span>${e.key}<div style="${this.cursor}" id="cursor"></div></span>`;
-                for (let i = 0; i < this.splitCmd.length; i++) {
+                for (let i = 0; i < this.splitCmd.length; i++)
                     if (this.splitCmd[i].includes("cursor")) {
                         if (i != 0) this.splitCmd.splice(i + 1, 0, span);
-                        else this.splitCmd.push(span);
+                        else if (this.splitCmd[i + 1]) this.splitCmd.splice(1, 0, span)
+                        else this.splitCmd.push(span)
                         this.splitCmd[i] = this.splitCmd[i].replace(
                             this.splitCmd[i].slice(
                                 this.splitCmd[i].indexOf("<div"),
@@ -81,7 +86,6 @@ export default {
                         this.cmd = str.join('')
                         return;
                     }
-                }
             }
         },
         backspace() {
